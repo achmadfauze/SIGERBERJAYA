49e21c1d01b4a2a584648cedc4ca03b4d2a3d3b9
achmadfauze <achmadfauzy464@gmail.com>
2022-09-19 20:20:51 +0700

    second commit

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -26,7 +26,7 @@ class LoginPage extends StatelessWidget {
                               builder: (context) => OnBoardPage()));
                     },
                     child: const Text(
-                      "Lewati",
+                      "Lewatiiiiii",
                       style: TextStyle(
                         color: Colors.white,
                         fontFamily: 'Roboto-Regular',
