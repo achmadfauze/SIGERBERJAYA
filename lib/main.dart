fce7bc239bae91c965254baa9c01e1dceb074bb7
unknown <achmadfauzy464@gmail.com>
2022-09-26 09:46:26 +0700

    snn

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:first_app/view/page/login.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/services.dart'; 
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(FirstApp());
 }
-
 class FirstApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
